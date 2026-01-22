c3a87d031764da0a8e4927d3a2e3bda791b0f625
Luis Gerardo Calderon Contreras <luisg.calderon@devcalcon.com>
2026-01-22 13:49:46 -0600

    Add pattern match

--- Chapter03/Operators/Program.cs
+++ Chapter03/Operators/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Pattern Matching
+// Add and remove the " " to change between string and int
+object o = "3";
+int j = 4;
+if ( o is int i)
+{
+    WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+    WriteLine("o is not an int so it cannot multiply!");
+
+}
