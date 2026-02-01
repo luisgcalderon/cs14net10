492be6507704d9c818645790dce816e111cd20f7
Luis Gerardo Calderon Contreras <luisg.calderon@devcalcon.com>
2026-02-01 08:44:19 -0600

    Debugging

--- Chapter04/Debugging/Program.cs
+++ Chapter04/Debugging/Program.cs
@@ -1,1 +1,11 @@
 ﻿//Debugging
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press Enter to end the app.");
+ReadLine(); // Wait for user to press Enter.
+double Add(double a, double b)
+{
+    return a + b; // Deliberatd bug!
+}
