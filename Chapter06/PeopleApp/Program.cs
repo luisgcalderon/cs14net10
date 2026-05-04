cc72b49b03ca5bfc2d01d180c5d51f8e71318670
Luis Calderon <luisg.calderon@hotmail.com>
2026-05-04 17:57:03 -0500

    Add generics

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -64,3 +64,13 @@ Console.WriteLine(format: "Key {0} has value: {1}",
 Console.WriteLine(format: "Key {0} has value: {1}",
 	arg0: harry,
 	arg1: lookupObject[harry]);
+// Define a Generic lookup collection.
+Dictionary<int, string> lookupIntString = new Dictionary<int, string>();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+lookupIntString.Add(key: 4, value: "Delta");
+key = 3;
+Console.WriteLine(format:"Key {0} has value: {1}",
+	arg0: key,
+	arg1: lookupIntString[key]);
